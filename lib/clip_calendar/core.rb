28a0c94b36c0afa4c5427509ad3a37abcfa96898
Tomoko Isobe <isobe@humancrest.co.jp>
2021-09-22 12:00:03 +0900

    課題2-3: 終了日の省略の実行コードを追加

--- lib/clip_calendar/core.rb
+++ lib/clip_calendar/core.rb
@@ -10,9 +10,16 @@ module ClipCalendar
     def initialize(argv)
 
       # 引数チェック
-      raise ArgumentNumberError, "### Wrong numbers of input (expected: 2, actual: #{argv.count})"  unless argv.count == 2
       begin
-        @dates= DateFormat.parse(argv[0])..DateFormat.parse(argv[1])
+        case argv.count
+        when 1
+          start_date= DateFormat.parse(argv[0])
+          @dates= start_date .. (start_date+5)
+        when 2
+          @dates= DateFormat.parse(argv[0])..DateFormat.parse(argv[1])
+        else
+          raise ArgumentNumberError, "### Wrong numbers of input (expected: 2, actual: #{argv.count})"
+        end
       rescue ArgumentError
         raise ArgumentTypeError, "### invalid argment type: "
       end
