f1f6ebdb1254c1a41ecadc3e736a71d3646cf578
Tomoko Isobe <isobe@humancrest.co.jp>
2021-09-22 11:50:31 +0900

    課題2-3: 終了日を省略したテストを追加

--- spec/clip_calendar/core_spec.rb
+++ spec/clip_calendar/core_spec.rb
@@ -3,14 +3,21 @@ RSpec.describe ClipCalendar::Core do
     context "正しい入力" do
       subject{ ClipCalendar::Core.new(input).output }
 
-      context "READMEの例" do
-        let(:input){ ["2020-05-18","2020-05-22"] }
-        it{ is_expected.to eq("2020/05/18(月)\n2020/05/19(火)\n2020/05/20(水)\n2020/05/21(木)\n2020/05/22(金)") }
+      context "開始日・終了日を入力" do
+        context "READMEの例" do
+          let(:input){ ["2020-05-18","2020-05-22"] }
+          it{ is_expected.to eq("2020/05/18(月)\n2020/05/19(火)\n2020/05/20(水)\n2020/05/21(木)\n2020/05/22(金)") }
+        end
+
+        context "同日を指定" do
+          let(:input){ ["2020-05-18","2020-05-18"] }
+          it{ is_expected.to eq("2020/05/18(月)") }
+        end
       end
 
-      context "同日を指定" do
-        let(:input){ ["2020-05-18","2020-05-18"] }
-        it{ is_expected.to eq("2020/05/18(月)") }
+      context "終了日を省略" do
+        let(:input){ ["2020-05-18"] }
+        it{ is_expected.to eq("2020/05/18(月)\n2020/05/19(火)\n2020/05/20(水)\n2020/05/21(木)\n2020/05/22(金)\n2020/05/23(土)") }
       end
     end
 
