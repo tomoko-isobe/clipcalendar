7a05bc736354d1846df6471518e243e6ad288529
Tomoko Isobe <isobe@humancrest.co.jp>
2021-09-22 11:41:47 +0900

    課題2-2: parseのテストパターンを追加

--- spec/clip_calendar/date_format_spec.rb
+++ spec/clip_calendar/date_format_spec.rb
@@ -3,13 +3,13 @@ RSpec.describe ClipCalendar::DateFormat do
     subject{ ClipCalendar::DateFormat.parse(input) }
 
     context "年月日" do
-      let(:input){ "2020-05-18" }
-      it { is_expected.to eq Date.new(2020,5,18) }
+      context("「-」区切り"){ let(:input){ "2020-05-18" }; it { is_expected.to eq Date.new(2020,5,18) } }
+      context("「/」区切り"){ let(:input){ "2020/05/18" }; it { is_expected.to eq Date.new(2020,5,18) } }
     end
 
     context "年の省略" do
-      let(:input){ "05-18" }
-      it { is_expected.to eq Date.new(Date.today.year,5,18) }
+      context("「-」区切り"){ let(:input){ "05-18" }; it { is_expected.to eq Date.new(Date.today.year,5,18) } }
+      context("「/」区切り"){ let(:input){ "05/18" }; it { is_expected.to eq Date.new(Date.today.year,5,18) } }
     end
   end
 end
