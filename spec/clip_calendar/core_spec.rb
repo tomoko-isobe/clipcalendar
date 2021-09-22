25e53d386d1064839b9bb94d21486974f2547a06
Tomoko Isobe <isobe@humancrest.co.jp>
2021-09-22 12:22:11 +0900

    課題2-4: 日付の省略のテストコードを追加、既存の矛盾するテストは削除

--- spec/clip_calendar/core_spec.rb
+++ spec/clip_calendar/core_spec.rb
@@ -19,12 +19,22 @@ RSpec.describe ClipCalendar::Core do
         let(:input){ ["2020-05-18"] }
         it{ is_expected.to eq("2020/05/18(月)\n2020/05/19(火)\n2020/05/20(水)\n2020/05/21(木)\n2020/05/22(金)\n2020/05/23(土)") }
       end
+
+      context "開始日・終了日を省略" do
+        let(:input){ [""] }
+        let(:expected){
+          today= Date.today
+          dates= today .. (today+5)
+          wdays= "日月火水木金土".split(//)
+          dates.map{|d| d.strftime("%Y/%M/%d")+ "("+ wdays[d.wday]+ ")"}.join("\n")
+        }
+        it{ is_expected.to eq(expected) }
+      end
     end
 
     context "誤った入力" do
       subject{ -> { ClipCalendar::Core.new(input) } }
 
-      context("引数が0個"){ let(:input){ [] }; it{ is_expected.to raise_error ClipCalendar::ArgumentNumberError } }
       context("引数が3個"){ let(:input){ ["2020-05-18","2020-05-22","2020-05-27"] }; it{ is_expected.to raise_error ClipCalendar::ArgumentNumberError } }
 
       context("日付形式が異なる"){ let(:input){ ["2020年05月18日","2020年05月22日"] }; it{ is_expected.to raise_error ClipCalendar::ArgumentTypeError } }
